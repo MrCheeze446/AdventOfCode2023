9d60070307bc1e017489757119345f5090a2b0f3
Anthony <mrcheeze12345@gmail.com>
2023-12-01 21:44:02 -0700

    part 2 close enough

--- Day_1_Py/main.py
+++ Day_1_Py/main.py
@@ -1,3 +1,28 @@
+
+def cleanDigits(digitArray: []):
+    cleanedDigits = []
+    for digit in digitArray:
+        if digit.isnumeric():
+            cleanedDigits.append(digit)
+
+    return cleanedDigits
+
+def getIndexes(inputArray, desiredElement):
+    return [index for index, element in enumerate(inputArray) if element == desiredElement]
+
+
+def checkIfSeen(givenLetter):
+    if givenLetter in timesSeen:
+        index = timesSeen.get(givenLetter)
+        digitIndex = getIndexes(inputString, givenLetter)[index]
+        timesSeen[givenLetter] += 1
+    else:
+        timesSeen.update({givenLetter: 1})
+        digitIndex = inputString.find(givenLetter)
+
+    return digitIndex
+
+
 if __name__ == '__main__':
     with open("input", "r") as fileInput:
         inputStrings = fileInput.read()
@@ -15,10 +40,12 @@ if __name__ == '__main__':
         "eight": "8",
         "nine": "9"
     }
+
+    timesSeen = {}
     twoDigitsList = []
 
     for inputString in inputList:
-        length = len(inputString)-1
+        length = len(inputString)
         digits = ['a'] * length
         for (key, value) in lettersToInt.items():
             if key in inputString:
@@ -26,15 +53,17 @@ if __name__ == '__main__':
                 digits[digitIndex] = value
         for letter in inputString:
             if letter.isnumeric():
-                digitIndex = inputString.find(letter)
+                digitIndex = checkIfSeen(letter)
                 digits[digitIndex] = letter
 
+        digits = cleanDigits(digits)
         if len(digits) == 1:
             twoDigits = digits[0] + digits[0]
         else:
             twoDigits = digits[0] + digits[len(digits) - 1]
 
         twoDigitsList.append(int(twoDigits))
+        timesSeen.clear()
         digits.clear()
 
     answer = 0
@@ -42,3 +71,4 @@ if __name__ == '__main__':
         answer += number
 
     print(answer)
+
